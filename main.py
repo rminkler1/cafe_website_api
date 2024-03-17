efd6d5d833cdd5e172fc973d906b60d4ee5e55c5
robertminkler <robertminkler@gmail.com>
2024-03-16 22:43:04 -0700

    add featured cafe

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 app = Flask(__name__)
 
 endpoint = "http://127.0.0.1:5123"
+featured_cafe = "The Slaughtered Lamb"
 
 
 def get_random_cafe():
@@ -12,12 +13,21 @@ def get_random_cafe():
     response.raise_for_status()
     return response.json()['cafe']
 
+
+def get_featured_cafe():
+    endpoint_url = endpoint + "/get_by_name?name=" + featured_cafe
+    response = requests.get(url=endpoint_url)
+    response.raise_for_status()
+    return response.json()['cafe']
+
+
 print(get_random_cafe())
 @app.route("/")
 def home():
     page_title = "Here goes the home page title"
     rand_cafe = get_random_cafe()
-    return render_template("index.html", page_title=page_title, r_cafe=rand_cafe)
+    featured = get_featured_cafe()
+    return render_template("index.html", page_title=page_title, f_cafe=featured, r_cafe=rand_cafe)
 
 
 @app.route("/search")
